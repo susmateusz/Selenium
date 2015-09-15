c655e2668b4e4fba6b8b4edb0e9d151177566656
MATSUS <susmateusz@gmail.com>
2015-09-15 22:45:29 +0200

    Added NewBookPage with empty Tests.

---
+++ src/test/java/starterkit/pages/impl/NewBookPage.java
@@ -0,0 +1,16 @@
+package starterkit.pages.impl;
+
+import org.openqa.selenium.WebDriver;
+import starterkit.pages.AbstractPageObject;
+
+/**
+ * Created by MATSUS on 15.09.2015.
+ */
+public class NewBookPage extends AbstractPageObject {
+
+    public NewBookPage(WebDriver driver) {
+        super(driver);
+    }
+
+
+}
---
+++ src/test/java/starterkit/tests/NewBookModalPageTest.java
@@ -0,0 +1,7 @@
+package starterkit.tests;
+
+/**
+ * Created by MATSUS on 15.09.2015.
+ */
+public class NewBookModalPageTest {
+}
