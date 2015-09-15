ad147ae2dfdcd52ccbb838b16035b5b9758a0bd6
MATSUS <susmateusz@gmail.com>
2015-09-15 22:49:29 +0200

    Empty tests for NewBookModalPage.

--- src/test/java/starterkit/pages/impl/NewBookPage.java
+++ src/test/java/starterkit/pages/impl/NewBookPage.java
@@ -1,16 +1,77 @@
 package starterkit.pages.impl;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import starterkit.pages.AbstractPageObject;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by MATSUS on 15.09.2015.
  */
 public class NewBookPage extends AbstractPageObject {
 
+    @FindBy(xpath = "//input [@ng-model='book.title']")
+    private WebElement bookTitle;
+
+    @FindBy(xpath = "//form/table")
+    private WebElement authorsTable;
+
+    @FindBy(xpath = "//input [@ng-model='firstName']")
+    private WebElement firstName;
+
+    @FindBy(xpath = "//input [@ng-model='lastName']")
+    private WebElement lastName;
+
+    @FindBy(xpath = "//button [@ng-click='addAuthor(firstName,lastName)']")
+    private WebElement addAuthorButton;
+
+    @FindBy(xpath = "/html/body/div[4]/div/div/div/form/div[3]/button[1]")
+    private WebElement addBookButton;
+
+    @FindBy(xpath = "//button [@id='modalAddBook']")
+    private WebElement flash;
+
     public NewBookPage(WebDriver driver) {
         super(driver);
     }
+
+    public NewBookPage setBookTitle(String title) {
+        bookTitle.clear();
+        bookTitle.sendKeys(title);
+        return this;
+    }
+    public NewBookPage setFirstName(String firstName) {
+        this.firstName.sendKeys(firstName);
+        return this;
+    }
+    public NewBookPage setLastName(String lastName) {
+        this.lastName.sendKeys(lastName);
+        return this;
+    }
+
+    public NewBookPage clickAddAuthorButton() {
+        addAuthorButton.click();
+        return this;
+    }
+
+    public NewBookPage clickAddBookButton() {
+        addBookButton.click();
+        return this;
+    }
+
+    public boolean isFlashDisplayed() {
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        return flash.isDisplayed();
+    }
+
+
+
+
+
+
 
 
 }
--- src/test/java/starterkit/tests/NewBookModalPageTest.java
+++ src/test/java/starterkit/tests/NewBookModalPageTest.java
@@ -1,7 +1,50 @@
 package starterkit.tests;
 
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import starterkit.pages.AbstractSelenium;
+import starterkit.pages.impl.NewBookPage;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by MATSUS on 15.09.2015.
  */
-public class NewBookModalPageTest {
+public class NewBookModalPageTest extends AbstractSelenium {
+
+    private NewBookPage newBookPage;
+
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+        newBookPage = openHomePage().clickBookList().clickAddBookButton();
+    }
+
+    @Test
+    @Ignore
+    public void testAddBookCorrectly() throws Exception {
+        // given
+        String title = "title";
+        String firstName = "firstName";
+        String lastName = "lastName";
+        // when
+        newBookPage.setBookTitle(title)
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .clickAddAuthorButton();
+        // then
+        assertFalse(newBookPage.hasError());
+    }
+
+    @Test
+    @Ignore
+    public void testShouldCheckIfTitleAndAuthorAreRequired() throws Exception {
+        // when
+        newBookPage.clickAddBookButton();
+        // then
+        assertTrue(newBookPage.isFlashDisplayed());
+    }
 }
