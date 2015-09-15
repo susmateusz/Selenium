7fc2391a559fe4a98c1b2f4ba89fe6e8fd825928
MATSUS <susmateusz@gmail.com>
2015-09-15 22:48:55 +0200

    Added tests for BookListPage.

---
+++ src/test/java/starterkit/pages/impl/BookListPage.java
@@ -0,0 +1,85 @@
+package starterkit.pages.impl;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import starterkit.pages.AbstractPageObject;
+
+import java.util.List;
+
+/**
+ * Created by MATSUS on 15.09.2015.
+ */
+public class BookListPage extends AbstractPageObject {
+
+    @FindBy(xpath = "//input")
+    private WebElement bookTitle;
+
+    @FindBy(xpath = "//button [text()='Szukaj']")
+    private WebElement searchButton;
+
+    @FindBy(xpath = "//button [@ng-click='addBook()']")
+    private WebElement addBookButton;
+
+    @FindBy(xpath = "//section/table")
+    private WebElement bookList;
+
+    @FindBy(xpath = "//button [text()='Edit']")
+    private List<WebElement> editButtons;
+
+    @FindBy(xpath = "//button [text()='Delete']")
+    private List<WebElement> deleteButtons;
+
+    @FindBy(className = "alert")
+    private WebElement flash;
+
+
+    public BookListPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public BookListPage setTitlePrefix(String title) {
+        this.bookTitle.sendKeys(title);
+        return this;
+    }
+
+    public BookListPage clickSearchButton() {
+        searchButton.click();
+        return PageFactory.initElements(driver, BookListPage.class);
+    }
+
+    public NewBookPage clickAddBookButton() {
+        addBookButton.click();
+        return PageFactory.initElements(driver, NewBookPage.class);
+    }
+
+    public List<WebElement> getRows() {
+        return bookList.findElements(By.tagName("tr"));
+    }
+
+    public NewBookPage clickEditButton(int index) {
+        if( index < editButtons.size())
+            editButtons.get(index).click();
+        return PageFactory.initElements(driver,NewBookPage.class);
+    }
+
+    public BookListPage clickDeleteButton(int index) {
+        if (index < deleteButtons.size())
+            deleteButtons.get(index).click();
+        return PageFactory.initElements(driver, BookListPage.class);
+    }
+
+    public boolean isFlashDisplayed() {
+        return flash.isDisplayed();
+    }
+
+    public int countBooks() {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(bookList));
+        return driver.findElements(By.xpath("//table/tbody/tr")).size() - 1;
+    }
+}
---
+++ src/test/java/starterkit/tests/BookListPageTest.java
@@ -0,0 +1,108 @@
+package starterkit.tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import starterkit.pages.AbstractSelenium;
+import starterkit.pages.impl.BookListPage;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by MATSUS on 15.09.2015.
+ */
+public class BookListPageTest extends AbstractSelenium {
+
+    private BookListPage bookListPage;
+    private String exampleTitle = "xxxxxxxxxxxxxxxx";
+
+
+    @Override
+    @Before
+    public void setUp() {
+        super.setUp();
+        bookListPage = openHomePage().clickBookList();
+        addExampleBook(exampleTitle);
+    }
+
+    @Override
+    @After
+    public void tearDown() {
+        try {
+            bookListPage.clickDeleteButton(bookListPage.countBooks() - 1);
+        } catch (NullPointerException e) {
+            System.err.println("Hue hue hue");
+        }
+        super.tearDown();
+    }
+
+    @Test
+    public void testCheckIfBookIsRemoved() throws Exception {
+        // when
+        bookListPage.clickSearchButton();
+        int allBooksCount = bookListPage.countBooks();
+
+        bookListPage.clickDeleteButton(allBooksCount - 1);
+        int booksAfterDeletionCount = bookListPage.countBooks();
+        // then
+        assertEquals(allBooksCount - 1, booksAfterDeletionCount);
+        assertTrue(bookListPage.isFlashDisplayed());
+    }
+
+    @Test
+    public void testCheckIfFiltersWorksWithEmptyPrefix() throws Exception {
+        // given
+        String emptyPrefix = " ";
+        // when
+        bookListPage.clickSearchButton();
+        int allBooksCount = bookListPage.countBooks();
+
+        bookListPage.setTitlePrefix(emptyPrefix);
+        int filteredBooksCount = bookListPage.countBooks();
+
+        // then
+        assertTrue(allBooksCount > 0);
+        assertEquals(filteredBooksCount, allBooksCount);
+
+    }
+
+    @Test
+    public void testCheckIfFiltersWorksWithPrefix() throws Exception {
+        // when
+        bookListPage.clickSearchButton();
+        bookListPage.setTitlePrefix(exampleTitle);
+        int filteredBooksCount = bookListPage.countBooks();
+        // then
+        assertEquals(1, filteredBooksCount);
+        // after
+
+    }
+
+    @Test
+    public void testIfEditBookWorks() throws Exception {
+        // given
+        String newTitle = "yyyy";
+        // when
+        bookListPage.clickSearchButton();
+        bookListPage.clickEditButton(bookListPage.countBooks()-1).setBookTitle(newTitle).clickAddBookButton();
+        bookListPage.clickSearchButton();
+        int allBooksCount = bookListPage.getRows().size();
+        // then
+        assertNotNull(bookListPage.getRows().get(allBooksCount - 1).findElement(By.xpath("//td[text()='" + newTitle + "']")));
+        assertTrue(bookListPage.isFlashDisplayed());
+    }
+
+    private void addExampleBook(String title) {
+        String firstName = "firstName";
+        String lastName = "lastName";
+        bookListPage.clickAddBookButton().setBookTitle(title)
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .clickAddAuthorButton()
+                .clickAddBookButton();
+    }
+}
